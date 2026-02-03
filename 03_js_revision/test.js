9595f9e98af2bb252ff80849eeb5a25f2103f021
Abhishek singh <abhishek0051278@gmail.com>
2026-02-03 13:17:36 +0530

    learing and implementing setInterval(),setTimeout(),clearInterval(),clearTimeout()

---
+++ 03_js_revision/test.js
@@ -0,0 +1,53 @@
+
+let printMyName = function(){
+
+    // console.log("abhishek");
+}
+
+
+   let changeMe =setTimeout(printMyName,2000);
+
+
+// document.getElementById('stop').addEventListener('click',function(){
+
+
+//     clearTimeout( changeMe);
+//     console.log("stopped");
+// },false)
+
+
+
+
+
+//   let printDate ;
+   
+//  let getDate = function (){
+          
+//      console.log("abhishek", Date.now());
+//  }
+
+
+ 
+// document.getElementById('start').addEventListener("click",() => {
+
+//     if(!printDate){
+
+//         // printDate = setInterval(getDate,2000);
+//     }
+// },false)
+
+
+
+
+// document.getElementById('stop').addEventListener("click",function(){
+
+//     // clearInterval( printDate);
+//     printDate = null;
+//     console.log("stopped");
+// },false)
+
+
+
+
+
+
