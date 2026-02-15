d3756dd33842f944e6f4ece61d84e43983878eac
Abhishek singh <abhishek0051278@gmail.com>
2026-02-15 19:51:33 +0530

    creating a component just for fun

---
+++ 11_currencyConvertor/src/components/abhi.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// function Abhi() {
+//   return (
+//     <div>
+
+//         <h1> hello i am abhishek just enjoy sunday </h1>
+      
+//     </div>
+//   )
+// }
+
+// export default Abhi
