90b29fe1441dab59e898b8748ef87110a6738ee8
Abhishek singh <abhishek0051278@gmail.com>
2026-03-01 19:35:21 +0530

    add GET API integration with table rendering and proper state management

--- 20_restAPI/src/App.jsx
+++ 20_restAPI/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import {useState} from "react";
+ 
+ 
+ 
+ 
+ 
+ 
+ function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+
+  const [getData, setGetData] = useState(null);
+  const [getLoading, setGetLoading] = useState(false);
+  const [getError, setGetError] = useState("");
+
+  const handleGetApi = async () => {
+    setGetLoading(true);
+    setGetError("");
+
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) throw new Error("Failed");
+
+      const data = await res.json();
+      setGetData(data);
+    } catch (err) {
+      setGetError("GET API failed!");
+    } finally {
+      setGetLoading(false);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: "20px" }}>
+      <h2>API Demo Page</h2>
+
+      <button onClick={handleGetApi}>
+        {getLoading ? "Loading..." : "Call GET API"}
+      </button>
+
+      {getError && <p style={{ color: "red" }}>{getError}</p>}
+
+      {getData && (
+        <table border="1">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {getData.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
-export default App
+
+export default App;
