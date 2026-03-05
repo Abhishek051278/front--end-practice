9fa9af5905189a3424280930625e2044620bf749
Abhishek singh <abhishek0051278@gmail.com>
2026-03-05 20:41:14 +0530

    Learn how to use Redux , ReduxToolkit

---
+++ 21_reduxToolkit/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import AddTodo from './App/components/AddTodo'
+import Todo from './App/components/Todo'
+
+function App() {
+  
+
+  return (
+    <>
+      <AddTodo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
