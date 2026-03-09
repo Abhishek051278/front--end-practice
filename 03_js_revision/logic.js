7cd3b00620ca4a4f53eb55e4a3424eb59019cff3
Abhishek singh <abhishek0051278@gmail.com>
2026-03-09 14:25:00 +0530

    implement palindrome check using reverse string logic with and wihtout built-in function

--- 03_js_revision/logic.js
+++ 03_js_revision/logic.js
@@ -16,7 +16,7 @@
     return reversed ;
   }
 
-      console.log(reverseString("Abhishek")); 
+    //   console.log(reverseString("Abhishek")); 
 
 
 
@@ -30,7 +30,7 @@
         
      }
 
-      console.log(reverseStringBuiltIn("Ironman"));
+    //   console.log(reverseStringBuiltIn("Ironman"));
                                                            
 
 
@@ -39,3 +39,61 @@
      //  the join() method is used to convert an array into a string.
      //   It joins all elements of the array using a separator.
      // The separator is the value placed between each array element when they are joined.
+
+
+
+
+
+     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+     // Check Palindrome with manual method 
+
+        function checkPalindrome (str){
+
+            let reversed = "";
+
+            for ( i = str.length - 1; i >= 0 ; i--){
+
+                reversed += str[i];
+            }
+
+               return reversed === str ;
+
+        }
+
+        // console.log(checkPalindrome("racecar"));
+        // console.log(checkPalindrome("women"));
+        // console.log(checkPalindrome("abhihba"));
+
+
+        // let str = "madam";
+        // let reversed = "";
+        
+        // for (let i = str.length-1  ; i >= 0 ; i--   ) {
+        //             reversed += str[i];
+        //     }
+        
+        //     console.log(str === reversed);
+
+
+        // check palindrome using built in function 
+
+        
+        // check palindrome with buit in fucntion
+
+
+        function isPalindrome (str){
+            
+            reversed = str.split("").reverse().join("")
+
+
+            return  str === reversed ;
+        }
+        
+            
+
+        // console.log(isPalindrome("abhishek"));
+        // console.log(isPalindrome("racecar"));
+     //   console.log(isPalindrome("1223221"));
+        
